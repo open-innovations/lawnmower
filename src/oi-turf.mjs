74a2e8bd04524a2907c117ce424c785b19a6406e
Stuart Lowe <299787+slowe@users.noreply.github.com>
2023-12-05 14:38:21 +0000

    Revert

--- src/oi-turf.mjs
+++ src/oi-turf.mjs
@@ -1,4 +1,10 @@
 // Add extra features to export from turf to the list below
 export {
-  area
+  area,
+  bbox,
+  bboxPolygon,
+  points,
+  polygon,
+  pointsWithinPolygon,
+  intersect
 } from "@turf/turf";
